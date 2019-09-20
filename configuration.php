e6eeb57ff5b244bdd033f718e3799d1e15e9a933
Darshan <darshanvithani@gmail.com>
2019-09-20 16:57:35 +0530

    code commit webtv

---
+++ configuration.php
@@ -0,0 +1,11 @@
+<?php 
+$XCStreamHostUrl = "http://choiceip.tv";
+$XClogoLinkval = "./images/shacktv.png";
+$XCcopyrighttextval = "ShackTV";
+$XCcontactUslinkval = "https://www.shacksystems.com/contact.php";
+$XChelpLinkval = "http://my407.online/webfolder/";
+$XClicenseIsval = "19760331";
+$XClocalKey = "1970331";
+$XCsitetitleval = "ShackTV";
+$LegazyLicense = "IPTVWEBPLAY-c78b4c378de54cb65c16d64cdc8e663ab4a";
+?>
